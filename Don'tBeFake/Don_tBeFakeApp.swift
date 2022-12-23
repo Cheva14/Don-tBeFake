a5a98514e3de7314c747d382e54d9f1d167d9e96
Sebastian Torrejon Alonzo <torrejse@mail.gvsu.edu>
2022-12-23 15:00:10 -0500

    Upload Files

---
+++ Don'tBeFake/Don_tBeFakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Don_tBeFakeApp.swift
+//  Don'tBeFake
+//
+//  Created by Sebastian Torrejon on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Don_tBeFakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
